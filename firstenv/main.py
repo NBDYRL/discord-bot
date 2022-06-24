3664d0471dedc651f8359c7d01c2cde1c0b94f0f
NBDYRL <kriszpicurr@gmail.com>
2022-06-24 15:36:54 -0400

    first draft

---
+++ firstenv/main.py
@@ -0,0 +1,12 @@
+name = "zag"
+othername = "nbdy"
+
+
+def greeting(parameter_1, parameter_2):
+    print(f"Hello!{parameter_1}")
+    print(f"hello!{parameter_2}")
+
+
+greeting(name, othername)
+
+input("press enter button to close the window")
